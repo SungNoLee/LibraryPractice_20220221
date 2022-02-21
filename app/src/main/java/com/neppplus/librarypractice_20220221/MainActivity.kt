b61360a57b567941e58e4d8da175691c0ea40908
이 성 노 <kulbo0926@gmail.com>
2022-02-21 12:21:05 +0900

    사용하려는 권한을 태그로 명시  - CALL_PHONE은 추가 작업필요

--- app/src/main/java/com/neppplus/librarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220221/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.neppplus.librarypractice_20220221
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun setupEvents(){
+//        버튼 이외의, TextView, ImageView 등도 setOnClickListener로 이벤트 처리가 가능함.
+        imgProfile.setOnClickListener {
+
+        }
+
+        btnCall.setOnClickListener {
+            val myUri = Uri.parse("tel:01033337777")
+            val myIntent = Intent( Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+        }
+    }
+    fun setValues() {
+
     }
 }
