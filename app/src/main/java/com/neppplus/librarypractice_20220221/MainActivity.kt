1adf9cd854499dd6f6b1874e1ec2799a7adfa269
이 성 노 <kulbo0926@gmail.com>
2022-02-21 15:01:37 +0900

    실제 권한 물어보는 코드 작성

--- app/src/main/java/com/neppplus/librarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220221/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.neppplus.librarypractice_20220221
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -38,6 +40,10 @@ class MainActivity : AppCompatActivity() {
 
             }
 
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
         }
     }
     fun setValues() {
